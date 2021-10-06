e0786108ed157a54499255a872663e9191f1dccb
lukasz.lukasiewicz <lukasz.lukasiewicz@gmail.com>
2021-10-06 21:05:42 +0200

    Add animation to paragraph tag on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,11 @@ import Head from 'next/head'
 import Header from 'components/Header/Header'
 import AnimatedHeader from 'components/AnimatedHeader/AnimatedHeader'
 import usePageConfig from 'hooks/usePageConfig'
+import { useRouter } from 'next/router'
+import { motion } from 'framer-motion'
 
 const Home: NextPage = () => {
+  const { locale } = useRouter()
   const page = usePageConfig()
   return (
     <>
@@ -16,7 +19,9 @@ const Home: NextPage = () => {
         color: page.color, minHeight: "100vh"
       }}>
         <AnimatedHeader initial="hidden" animate="visible" split="letter">Hi ;)</AnimatedHeader>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet dolorum quia, molestiae dolores iusto accusantium ipsa illum fugit hic at veritatis repellendus eum placeat perferendis nesciunt ipsam ullam, error odio!</p>
+        <motion.div key={locale + "p"} initial={{ y: "-4em", opacity: 0 }} animate={{ y: 0, opacity: 1 }} exit={{ y: "4em" }} transition={{ type: "spring", delay: .3 }}>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ipsam delectus nobis soluta officia reprehenderit ducimus praesentium nesciunt rerum at est omnis, itaque velit libero optio amet vitae iure eligendi.</p>
+        </motion.div>
       </Header>
     </>
 
