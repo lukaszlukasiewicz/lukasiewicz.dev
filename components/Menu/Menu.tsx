6dd651a4c695d236165d3db69a99a76b3b17851b
lukasz.lukasiewicz <lukasz.lukasiewicz@gmail.com>
2021-10-08 20:31:54 +0200

    Set scroll to false in menu links & change menu z-index

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -13,14 +13,14 @@ const Menu = () => {
   return <nav className={Styles.Menu}>
     <div style={{ color: currentPage?.color, borderBottomColor: currentPage?.color + "22" }}>
       <div className={Styles.Menu__link}>
-        <Link href="/">
+        <Link scroll={false} href="/">
           <a>lukasiewicz.dev</a>
         </Link>
       </div>
       {pages.filter(entry => !!entry.inMenu).map(entry => {
         const { path, name } = entry
         const isActive = pathname === path
-        return <Link href={path} key={name[currentLocale]}>
+        return <Link scroll={false} href={path} key={name[currentLocale]}>
           <a className={`${Styles['Menu__link']} ${isActive && Styles['Menu__link--active']}`}>{name[currentLocale]}</a>
         </Link>
       })}
