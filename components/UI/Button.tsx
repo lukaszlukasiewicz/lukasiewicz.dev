e18513306df79366641b3f1943f71ace2a7bd616
lukasz.lukasiewicz <lukasz.lukasiewicz@gmail.com>
2021-10-09 20:27:53 +0200

    Add useRef to prevent changing colors before page change

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -1,5 +1,7 @@
 import usePageConfig from "hooks/usePageConfig"
+import { useEffect, useMemo, useRef } from "react"
 import Styles from './Button.module.scss'
+import { Page } from "config/pages"
 
 type ButtonProps = {
   color?: string,
@@ -15,23 +17,26 @@ export const Button: React.FC<ButtonProps & React.HTMLProps<HTMLButtonElement>> 
 
 
 export const DefaultButton: React.FC<React.HTMLProps<HTMLButtonElement> & ButtonProps> = ({ children, ...rest }) => {
-  const page = usePageConfig()
-  return <Button color={page.backgroundColor} backgroundColor={page.color} {...rest}>
+  const page = useRef(usePageConfig());
+  const { color, backgroundColor } = page.current;
+  return <Button color={backgroundColor} backgroundColor={color} {...rest}>
     {children}
   </Button>
 }
 
 
 export const NegativeButton: React.FC<React.HTMLProps<HTMLButtonElement> & ButtonProps> = ({ children, ...rest }) => {
-  const page = usePageConfig()
-  return <Button color={page.color} backgroundColor={page.backgroundColor} {...rest}>
+  const page = useRef(usePageConfig());
+  const { color, backgroundColor } = page.current;
+  return <Button color={color} backgroundColor={backgroundColor} {...rest}>
     {children}
   </Button>
 }
 
 export const PrimaryButton: React.FC<ButtonProps & React.HTMLProps<HTMLButtonElement>> = ({ children, ...rest }) => {
-  const page = usePageConfig()
-  return <Button color={page.primaryBackgroundColor} backgroundColor={page.primaryBackgroundColor ?? page.primaryColor} {...rest}>
+  const page = useRef(usePageConfig());
+  const { primaryColor, primaryBackgroundColor } = page.current;
+  return <Button color={primaryBackgroundColor} backgroundColor={primaryBackgroundColor ?? primaryColor} {...rest}>
     {children}
   </Button>
 }
