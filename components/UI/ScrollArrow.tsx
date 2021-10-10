e97e25c549f10e443e109da1e0f14f700bd80571
lukasz.lukasiewicz <lukasz.lukasiewicz@gmail.com>
2021-10-10 19:19:48 +0200

    Remove repeat delay from ScrollArrow animation

--- components/UI/ScrollArrow.tsx
+++ components/UI/ScrollArrow.tsx
@@ -4,7 +4,7 @@ import Styles from "./ScrollArrow.module.scss"
 
 const ScrollArrow = () => {
   return <motion.div className={Styles.ScrollArrow} initial={{ y: "-2em", opacity: 0 }} animate={{ y: 0, opacity: 1, transition: { delay: .5, duration: 1, ease: "backOut" } }} exit={{ y: "2em" }} transition={{ duration: 1, ease: "backOut" }}>
-    <motion.div animate={{ y: ["-.5em", "-1em", "0em", "-1em", "0em", "-1em", "-.5em"] }} transition={{ type: "spring", stiffness: 100, duration: 3, delay: 1, repeat: Infinity, repeatDelay: 2 }}>
+    <motion.div animate={{ y: ["-1em", "0em", "-1em", "0em", "-1em"] }} transition={{ type: "spring", stiffness: 100, duration: 3, delay: 2, repeat: Infinity }}>
       <BsArrowDown />
     </motion.div>
   </motion.div>
