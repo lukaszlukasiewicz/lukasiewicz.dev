83c5087650879e5a04f6707578197515c7603982
lukasz.lukasiewicz <lukasz.lukasiewicz@gmail.com>
2021-10-12 10:46:06 +0200

    Move custom fonts to _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import usePageConfig from 'hooks/usePageConfig'
 import Menu from 'components/Menu/Menu'
 import { AnimatePresence } from 'framer-motion'
 
