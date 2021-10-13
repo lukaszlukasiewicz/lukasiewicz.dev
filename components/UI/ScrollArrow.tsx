8b0913fb300e72b8ab13c5bca479d4aea8f4ec6e
lukasz.lukasiewicz <lukasz.lukasiewicz@gmail.com>
2021-10-13 10:40:58 +0200

    Adjust ScrollArrow animation

--- components/UI/ScrollArrow.tsx
+++ components/UI/ScrollArrow.tsx
@@ -3,7 +3,7 @@ import { BsArrowDown } from "react-icons/bs"
 import Styles from "./ScrollArrow.module.scss"
 
 const ScrollArrow = () => {
-  return <motion.div className={Styles.ScrollArrow} initial={{ y: "-2em", opacity: 0 }} animate={{ y: 0, opacity: 1, transition: { delay: .5, duration: 1, ease: "backOut" } }} exit={{ y: "2em" }} transition={{ duration: 1, ease: "backOut" }}>
+  return <motion.div className={Styles.ScrollArrow} initial={{ y: "-2em", opacity: 0 }} animate={{ y: 0, opacity: 1, transition: { delay: .5, duration: 1, ease: "backOut" } }} exit={{ y: "2em", opacity: 0 }} transition={{ duration: 1, ease: "backOut" }}>
     <motion.div animate={{ y: ["-1em", "0em", "-1em", "0em", "-1em"] }} transition={{ type: "spring", stiffness: 100, duration: 3, delay: 2, repeat: Infinity }}>
       <BsArrowDown />
     </motion.div>
