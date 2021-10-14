4b3681ce81566739a23f3c425b96c4d96b80011a
lukasz.lukasiewicz <lukasz.lukasiewicz@gmail.com>
2021-10-14 15:10:53 +0200

    Add Fira Code font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,300;0,600;0,800;1,300;1,600;1,800&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;500&Fira+Sans:ital,wght@0,300;0,600;0,800;1,300;1,600;1,800&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
