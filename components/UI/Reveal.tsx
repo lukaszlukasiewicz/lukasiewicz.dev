16a74450442711cd27e1cf5783404fff4be0b902
lukasz.lukasiewicz <lukasz.lukasiewicz@gmail.com>
2021-10-17 12:39:26 +0200

    Add color option to Reveal component

--- components/UI/Reveal.tsx
+++ components/UI/Reveal.tsx
@@ -5,27 +5,27 @@ import { useEffect, useRef } from "react"
 
 const wrapperVariants = {
   visible: { y: 0, scaleY: 1, transition: { ease: "easeOut", duration: .5, delay: .5 } },
-  hidden: { y: "-4em", scaleY: 0 }
+  hidden: { y: "4em", scaleY: 0, transition: { ease: "easeOut" } }
 }
 
 const coverVariants = {
   visible: { scaleY: 0, transition: { ease: "easeOut", delay: 1 } },
-  hidden: { scaleY: 1 }
+  hidden: { scaleY: 1, transition: { ease: "easeOut" } }
 }
 
-const Reveal: React.FC<{ className?: string }> = ({ className = "", children }) => {
+const Reveal: React.FC<{ className?: string, color?: string }> = ({ className = "", color, children }) => {
   const wrapperRef = useRef<HTMLDivElement>(null)
   const controls = useAnimation()
   const [observe, unObserve] = useIntersectionObserver()
 
   useEffect(() => {
     const current = wrapperRef.current
-    observe(current, () => controls.start("visible"), (entry: IntersectionObserverEntry) => { console.log(entry); if (entry.boundingClientRect.top > 0) controls.start("hidden") })
+    observe(current as HTMLElement, () => controls.start("visible"), (entry: IntersectionObserverEntry) => { console.log(entry); if (entry.boundingClientRect.top > 0) controls.start("hidden") })
   }, [observe, unObserve, controls])
 
   return <div ref={wrapperRef}>
     <motion.div variants={wrapperVariants} animate={controls} initial="hidden" className={[Styles.Reveal, className].join(" ")}>
-      <motion.div variants={coverVariants} className={Styles.Reveal__cover}></motion.div>
+      <motion.div variants={coverVariants} className={Styles.Reveal__cover} style={{ backgroundColor: color }}></motion.div>
       {children}
     </motion.div>
   </div>
