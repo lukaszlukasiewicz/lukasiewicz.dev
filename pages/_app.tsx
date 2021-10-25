9ae83a685601e745bf471d91ba05bc8ffe281df8
lukasz.lukasiewicz <lukasz.lukasiewicz@gmail.com>
2021-10-25 18:01:50 +0200

    Fix for disapearing styles github.com/vercel/next.js/issues/17464

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,24 @@ import Menu from 'components/Menu/Menu'
 import { AnimatePresence } from 'framer-motion'
 import Cursor from 'components/Cursor/Cursor'
 import LockOut from 'components/LockOut/LockOut'
+import Router from "next/router";
+
+const routeChange = () => {
+  // Temporary fix to avoid flash of unstyled content
+  // during route transitions. Keep an eye on this
+  // issue and remove this code when resolved:
+  // https://github.com/vercel/next.js/issues/17464
+  const tempFix = () => {
+    const allStyleElems = document.querySelectorAll('style[media="x"]');
+    allStyleElems.forEach((elem) => {
+      elem.removeAttribute("media");
+    });
+  };
+  tempFix();
+};
+
+Router.events.on("routeChangeComplete", routeChange);
+Router.events.on("routeChangeStart", routeChange);
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return <LockOut>
