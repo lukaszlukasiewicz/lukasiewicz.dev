ebc4c328f1a6859d0184c989dc05007500c57ad3
Łukasz <lukasz@Air.local>
2026-07-01 23:12:00 +0200

    pocket fix

--- utils/getPocketPosts.ts
+++ utils/getPocketPosts.ts
@@ -4,31 +4,35 @@ import getJsonData from './getJsonData';
 const file = 'data/pocket.json'
 
 const updatePocketPosts = async () => {
-
-  const pocketData = await fetch("https://getpocket.com/v3/get", {
-    method: 'POST',
-    redirect: 'follow',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      "consumer_key": process.env.POCKET_CONSUMER_KEY,
-      "access_token": process.env.POCKET_ACCESS_TOKEN,
-      "tag": "reading",
-      "sort": "newest",
-      "count": 4
+  try{
+    const pocketData = await fetch("https://getpocket.com/v3/get", {
+      method: 'POST',
+      redirect: 'follow',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "consumer_key": process.env.POCKET_CONSUMER_KEY,
+        "access_token": process.env.POCKET_ACCESS_TOKEN,
+        "tag": "reading",
+        "sort": "newest",
+        "count": 4
+      })
     })
-  })
-  const pocketJson = await pocketData.json()
-  const data = {
-    updated: Date.now(),
-    posts: Object.values(pocketJson.list)
+    const pocketJson = await pocketData.json()
+    const data = {
+      updated: Date.now(),
+      posts: Object.values(pocketJson.list)
+    }
+    fs.writeFile(file, JSON.stringify(data), err => {
+      if (err) console.log("error writing file", file, err)
+    })
   }
-  fs.writeFile(file, JSON.stringify(data), err => {
-    if (err) console.log("error writing file", file, err)
-  })
+  catch (error) {
+    console.log("error fetching pocket posts", error)
+  }
 }
-
 const getPocketPosts = () => getJsonData(file, updatePocketPosts)
+
 
 export default getPocketPosts
